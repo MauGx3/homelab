836566ee4ca2ce051f92cdf8e29cf749717dc408
Maurício Gioachini <maugx3@gmail.com>
2025-08-16 16:31:37 -0300

    refactor: change to sh format

--- games/igir-library-sync.sh
+++ games/igir-library-sync.sh
@@ -1,45 +1,51 @@
-#!/usr/bin/env bash
+#!/bin/sh
 # @param {...string} $@ Input directories to merge into this collection
-set -euo pipefail
+set -eu
 
-# shellcheck disable=SC2064
-trap "cd \"${PWD}\"" EXIT
+# Save current working directory and restore on exit (POSIX-friendly)
+ORIG_PWD=$(pwd)
+trap 'cd "$ORIG_PWD"' EXIT
 cd "$(dirname "$0")"
 
+# Helper: shell-quote a string for safe eval usage (single-quote, escape existing single quotes)
+quote() {
+  # Uses sed to escape single quotes in the input
+  printf "'%s'" "$(printf '%s' "$1" | sed "s/'/'\\''/g")"
+}
 
-# Treat every CLI argument as an input directory
-INPUTS=()
+# Build input arguments: transform each positional argument into: --input 'ARG'
+INPUT_ARGS=
 for INPUT in "$@"; do
-  INPUTS+=(--input "${INPUT}")
+  INPUT_ARGS="$INPUT_ARGS --input $(quote "$INPUT")"
 done
 
 # Cartridge-based consoles, 1st-5th generations
-npx --yes igir@latest move zip test clean report \
-  --dat "./No-Intro*.zip" \
-  --dat-name-regex-exclude "/encrypted|source code/i" \
-  --input "./No-Intro/" \
-  "${INPUTS[@]:-}" \
-  `# Trust checksums in archive headers, don't checksum archives (we only care about the contents)` \
+# Trust checksums in archive headers, don't checksum archives (we only care about the contents)
+eval "npx --yes igir@latest move zip test clean report \
+  --dat './No-Intro*.zip' \
+  --dat-name-regex-exclude '/encrypted|source code/i' \
+  --input './No-Intro/' \
+  $INPUT_ARGS \
   --input-checksum-max CRC32 \
   --input-checksum-archives never \
-  --patch "./Patches/" \
-  --output "./No-Intro/" \
+  --patch './Patches/' \
+  --output './No-Intro/' \
   --dir-dat-name \
   --overwrite-invalid \
-  --zip-exclude "*.{chd,iso}" \
+  --zip-exclude '*.{chd,iso}' \
   --reader-threads 4 \
-  -v
+  -v"
 
 # Disc-based consoles, 4th+ generations
-npx --yes igir@latest move test clean report \
-  --dat "./Redump*.zip" \
-  --input "./Redump/" \
-  "${INPUTS[@]}" \
-  `# Let maxcso calculate CSO CRC32s, don't checksum compressed discs (we only care about the contents)` \
+# Let maxcso calculate CSO CRC32s, don't checksum compressed discs (we only care about the contents)
+eval "npx --yes igir@latest move test clean report \
+  --dat './Redump*.zip' \
+  --input './Redump/' \
+  $INPUT_ARGS \
   --input-checksum-max CRC32 \
   --input-checksum-archives never \
-  --patch "./Patches/" \
-  --output "./Redump/" \
+  --patch './Patches/' \
+  --output './Redump/' \
   --dir-dat-name \
   --overwrite-invalid \
   --only-retail \
@@ -47,19 +53,18 @@ npx --yes igir@latest move test clean report \
   --prefer-language EN \
   --prefer-region USA,WORLD,EUR,JPN \
   --prefer-revision newer \
-  -v
+  -v"
 
-npx --yes igir@latest move zip test clean \
-  `# Official MAME XML extracted from the progetto-SNAPS archive` \
-  --dat "./mame*.xml" \
-  `# Rollback DAT downloaded from Pleasuredome` \
-  --dat "./MAME*Rollback*.zip" \
-  --input "./MAME/" \
-  "${INPUTS[@]}" \
+# MAME (arcade) collection
+eval "npx --yes igir@latest move zip test clean \
+  --dat './mame*.xml' \
+  --dat './MAME*Rollback*.zip' \
+  --input './MAME/' \
+  $INPUT_ARGS \
   --input-checksum-quick \
   --input-checksum-archives never \
-  --output "./MAME/" \
+  --output './MAME/' \
   --dir-dat-name \
   --overwrite-invalid \
   --merge-roms merged \
-  -v
+  -v"
